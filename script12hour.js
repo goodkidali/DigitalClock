116d24154fe5b554b053c9c09139a8ef5d23cffd
Ali Saheb <alisaheb1234@gmail.com>
2023-08-14 02:18:12 -0400

    fixed 12 hour clock bug

--- script12hour.js
+++ script12hour.js
@@ -11,10 +11,13 @@ setInterval(() => {
     let day = allDays[date.getDay()]
     let month = allMonths[date.getMonth()]
     let number = date.getDate()
-    let hour = date.getHours()-12
+    let hour =  date.getHours()
     let min = date.getMinutes()
     let sec = date.getSeconds()
 
+    if(hour > 12) {
+        hour = hour - 12;
+    }
     if(hour < 10){
         hour = "0" + hour
     }
